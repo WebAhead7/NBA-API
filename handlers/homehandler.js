92a3290b0d47514b9a5c6a6a6d8de23c249c3c35
Muhammad Awwad <hammode.awad@gmail.com>
2020-11-12 14:15:55 +0200

    fixed some issues

--- handlers/homehandler.js
+++ handlers/homehandler.js
@@ -1,12 +1,11 @@
 const fs = require("fs");
 const path = require("path");
-
+const missingHandler = require("./missinghandler");
 function homeHandler(request, response) {
   const homePath = path.join(__dirname, "..", "public", "index.html");
   fs.readFile(homePath, "utf-8", (error, data) => {
     if (error) {
-      response.writeHead(404, { "content-type": "text/html" });
-      response.end("<h1>Something Went Wrong!</h1>");
+      missingHandler(request, response);
     } else {
       response.writeHead(200, { "content-type": "text/html" });
       response.end(data);
--- handlers/resourceshandler.js
+++ handlers/resourceshandler.js
@@ -1,27 +1,25 @@
-const fs=require("fs");
-const path=require("path");
-
-const TYPES={
-    ".html":"text/html",
-    ".css":"text/css",
-    ".js":"application/javascript",
-    ".png":"image/png",
-    ".jpg":"image/jpg"
-}
-function resourcesHandler(request,response){
-    const url=request.url;
-    const filePath=path.join(__dirname,"..",url);
-    const contentType=TYPES[path.extname(filePath)];
-fs.readFile(filePath,"utf-8",(error,data)=>{
-    if(error){
-        response.writeHead(404,{"content-type":"text/html"});
-        response.end("<h1>Not Found</h1>");
+const fs = require("fs");
+const path = require("path");
+const missingHandler = require("./missinghandler");
+const TYPES = {
+  ".html": "text/html",
+  ".css": "text/css",
+  ".js": "application/javascript",
+  ".png": "image/png",
+  ".jpg": "image/jpg",
+};
+function resourcesHandler(request, response) {
+  const url = request.url;
+  const filePath = path.join(__dirname, "..", url);
+  const contentType = TYPES[path.extname(filePath)];
+  fs.readFile(filePath, "utf-8", (error, data) => {
+    if (error) {
+      missingHandler(request, response);
+    } else {
+      response.writeHead(200, { "content-type": contentType });
+      response.end(data);
     }
-    else{
-        response.writeHead(200,{"content-type":contentType});
-        response.end(data);
-    }
-})
+  });
 }
 
-module.exports=resourcesHandler;
+module.exports = resourcesHandler;
